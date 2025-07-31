931391a40181a965f42f2c198167885d3afc2616
orhtej2 <2871798+orhtej2@users.noreply.github.com>
2025-08-01 00:14:38 +0200

    Update _common.sh

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -6,5 +6,5 @@
 
 nodejs_version=21
 current_hash="16cf16c4"
-version="2.7.2-10698-g16cf16c4"
-build_date="Sun, 29 Sep 2024 09:14:03 +0000"
+version="2.8.1-10698-g16cf16c4"
+build_date="Tue, 29 Jul 09:14:03 +0000"
